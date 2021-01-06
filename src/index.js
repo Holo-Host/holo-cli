6a68f5478225ded71bbf95019f6f05a193883b23
Matthew Brisebois <matthew@webheroes.ca>
2021-01-05 20:52:49 -0700

    conductor-cli: add commands 'gen-agent', 'install', 'activate', 'attach-interface', 'call-zome'

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const log				= require('@whi/stdlog')(path.basename( __filename ), {
 const crypto				= require('crypto');
 const print				= require('@whi/printf').colorAlways();
 const { Command }			= require('commander');
-const { AdminWebsocket, ...sdk }	= require('@holochain/conductor-api');
+const { AdminWebsocket,
+	AppWebsocket,
+	...sdk }			= require('@holochain/conductor-api');
 
 const config				= require('./config.js');
 
@@ -17,9 +19,10 @@ function increaseTotal(v, total) {
     return total + 1;
 }
 
-async function execute (async_fn) {
+let port;
+async function execute_admin (async_fn) {
     log.info("Create client");
-    const admin			= await AdminWebsocket.connect( "http://localhost:33001" );
+    const admin			= await AdminWebsocket.connect( "http://localhost:" + port );
 
     try {
 	await async_fn(admin);
@@ -33,8 +36,23 @@ async function execute (async_fn) {
     }
 }
 
+async function execute_app (port, async_fn) {
+    log.info("Create client");
+    const client		= await AppWebsocket.connect( "http://localhost:" + port );
+
+    try {
+	await async_fn(client);
+    } catch ( err ) {
+	console.error(`Failed during execute for '${async_fn.name}':`, err );
+    } finally {
+	client.client.socket.terminate();
+	await client.client.awaitClose();
+    }
+}
+
 function prelude (handler) {
     return function ( ...args ) {
+	port			= this.parent.port;
 	log.transports[0].setLevel( this.parent.verbose );
 	handler.call(this, ...args );
     };
@@ -46,14 +64,15 @@ async function main ( args ) {
 
     commander
 	.version( config.version )
-	.option('-q, --quiet', 'Suppress all printing except for final result', false )
-	.option('-v, --verbose', 'Increase logging verbosity', increaseTotal, 0);
+	.option('-q, --quiet', 'suppress all printing except for final result', false )
+	.option('-v, --verbose', 'increase logging verbosity', increaseTotal, 0)
+	.option('-p, --port <port>', 'admin port', 33001 )
 
     commander
 	.command("list <type>")
-	.description("List the current configuration for the given 'type' (dnas, cells, apps)")
+	.description("list the current configuration for the given 'type' (dnas, cells, apps)")
 	.action( prelude(function (type) {
-	    execute(async (admin) => {
+	    execute_admin(async (admin) => {
 		let results;
 
 		if ( ["dna", "dnas"].includes(type) ) {
@@ -73,22 +92,115 @@ async function main ( args ) {
 	    });
 	}));
 
+
+    async function gen_pubkey ( client ) {
+	const pubkey		= await client.generateAgentPubKey();
+	return pubkey;
+    }
+
     commander
-	.command("install <dna>")
-	.description("Install app ")
-	.action( prelude(function (dna) {
-	    console.log( dna );
-	    // let install_request = {
-	    // 	"installed_app_id": "notes",
-	    // 	"agent_key": agent_pubkey,
-	    // 	"dnas": [{
-	    // 	    "path": path.resolve( __dirname, "../dnas/notes.dna.gz" ),
-	    // 	    "nick": "holofuel",
-	    // 	}],
-	    // };
-	    //
-	    // execute(async (admin) => {
-	    // });
+	.command("gen-agent")
+	.description("generate a new agent pubkey")
+	.action( prelude(function () {
+	    execute_admin(async (admin) => {
+		const pubkey		= await admin.generateAgentPubKey();
+		console.log( pubkey.toString("base64") );
+	    });
+	}));
+
+    commander
+	.command("install <name> <dna...>")
+	.description("install app")
+	.option('-a, --agent [pubkey]', 'create a new agent for installed App', false)
+	.action( prelude(function ( name, dnas ) {
+	    execute_admin(async (admin) => {
+		let agent_pubkey;
+
+		if ( this.agent === false ) {
+		    agent_pubkey		= await gen_pubkey( admin );
+		    log.normal("Using Agent (%s) for install", agent_pubkey.toString("base64") );
+		}
+		else {
+		    agent_pubkey		= Buffer.from( this.agent, "base64" );
+		    log.normal("Using Agent (%s) for install", this.agent );
+		}
+
+		console.log( dnas );
+		let install_request = {
+		    "installed_app_id": name,
+		    "agent_key": agent_pubkey,
+		    "dnas": dnas.map(dna => {
+			const segs = dna.split(":");
+			const file_path = segs.slice(0,-1).join("/");
+			const nick = segs.slice(-1);
+
+			return {
+			    "path": path.resolve( process.cwd(), file_path ),
+			    "nick": "holofuel",
+			};
+		    }),
+		};
+
+		console.log( install_request );
+		let app = await admin.installApp( install_request );
+
+		console.log( app.installed_app_id );
+		app.cell_data.map(config => {
+		    let cell = config[0];
+		    let nick = config[1];
+		    let hash = cell[0].toString("base64");
+		    let agent = cell[1].toString("base64");
+
+		    console.log(`  ${nick}\n    ${hash}:${agent}`);
+		});
+	    });
+	}));
+
+    commander
+	.command("activate <name>")
+	.description("activate app")
+	.action( prelude(function ( name ) {
+	    execute_admin(async (admin) => {
+		await admin.activateApp({
+		    "installed_app_id": name,
+		});
+
+		console.log("Hurray! Let's assume this worked.  What could go wrong?");
+	    });
+	}));
+
+    commander
+	.command("attach-interface <port>")
+	.description("attach app to TCP port")
+	.action( prelude(function ( port ) {
+	    execute_admin(async (admin) => {
+		let resp = await admin.attachAppInterface({
+		    "port": parseInt( port ),
+		});
+
+		console.log(`This port (${resp.port}) is now something`);
+	    });
+	}));
+
+    commander
+	.command("call-zome <port> <dna> <agent>")
+	.description("call a zome function")
+	.action( prelude(function ( port, dna, agent ) {
+	    execute_app( port, async (client) => {
+		let dna_hash		= Buffer.from( dna, "base64" );
+		let agent_pubkey	= Buffer.from( agent, "base64" );
+
+		let call_spec = {
+		    "cell_id": [ dna_hash, agent_pubkey ],
+		    "zome_name": "hha",
+		    "fn_name": "get_happs",
+		    "payload": null,
+		    "provenance": agent_pubkey, // AgentPubKey
+		};
+
+		let resp = await client.callZome( call_spec );
+		console.log( resp );
+	    });
 	}));
 
     commander.parse( args );
